c7eb244de602a38db70d7cef5c7571537df73cce
Ben Foxall <benfoxall@gmail.com>
2013-08-05 23:11:21 +0100

    let example look at any site in THE ENTIRE WORLD

--- examples/link-areas.js
+++ examples/link-areas.js
@@ -2,27 +2,41 @@ var PORT = require('system').env.PORT || 8080;
 
 var server = require('webserver').create();
 server.listen(PORT, function(request, response) {
-  var page = new WebPage();
-  page.open('http://benjaminbenben.com', function (status) {
 
-    var links = page.evaluate(function () {
-      return [].reduce.call(document.querySelectorAll('a'), function(memo, a){
-        var bb = a.getBoundingClientRect(),
-          area = bb.width * bb.height,
-          href = a.href
+  var url = request.url;
+  var idx = url.indexOf('?url=');
+  if(idx != -1){
+    var stripped = unescape(url.substr(idx + 5));
+    console.log(stripped);
 
-        if(area)
-          memo[href] = (memo[href] || 0) + area;
-       
-        return memo;
-      },{});
+    var page = new WebPage();
+    page.open(stripped, function (status) {
+
+      var links = page.evaluate(function () {
+        return [].reduce.call(document.querySelectorAll('a'), function(memo, a){
+          var bb = a.getBoundingClientRect(),
+            area = bb.width * bb.height,
+            href = a.href;
+
+          if(area)
+            memo[href] = (memo[href] || 0) + area;
+         
+          return memo;
+        },{});
+      });
+
+      response.setHeader('Content-Type', 'application/json; charset=utf-8');
+      response.setHeader('Access-Control-Allow-Origin','*');
+      response.statusCode = 200;
+      response.write(JSON.stringify(links));
+      response.close();
+      page.close();
     });
 
-    response.setHeader('Content-Type', 'application/json; charset=utf-8');
-    response.setHeader('Access-Control-Allow-Origin','*');
+  } else {
     response.statusCode = 200;
-    response.write(JSON.stringify(links));
+    response.write('nope');
     response.close();
-    page.close();
-  });
+  }
+
 });
